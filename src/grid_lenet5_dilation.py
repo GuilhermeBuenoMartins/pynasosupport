c23b845a386860bf849ea28d23bc5d8adb8a3106
Guilherme Bueno Martins <gui.bue@uni9.edu.br>
2022-10-14 15:11:08 -0300

    Creation of function main.

--- src/grid_lenet5_dilation.py
+++ src/grid_lenet5_dilation.py
@@ -5,6 +5,7 @@ Script executes gridsearch in LeDilationNet-5 model.
 """
 
 import logging
+import os.path
 from datetime import datetime
 
 import tensorflow as tf
@@ -16,57 +17,61 @@ from functions.media import read_imgs
 from functions.model import get_lenet5
 from functions.sampler import build_sets
 
+def main(main_path:str, factor=.9, list_num_filter=[4, 8, 12, 16], list_kernel_size=[(3,3), (5,5)], batch_size=None, epochs=100, train_prcnt=2/3, verbose=1):
+    """
+    Main function to execution the script.
 
-# Constants
+    :param main_path:
+    :param factor:
+    :param list_num_filter:
+    :param list_kernel_size:
+    :param batch_size:
+    :param epochs:
+    :param train_prcnt:
+    :return:
+    """
 
-# FILE_LOG = '../output/logs/grid_lenet5_dilation{}.log'.format(datetime.now().strftime('%Y-%m-%d_%H-%M%S'))
-FILE_NAME = 'grid_lenet5_dilation{}.log'.format(datetime.now().strftime('%Y-%m-%d_%H-%M%S'))
-NEGATIVE_PATH = '/home/gmartins/arquivos/uninove/mestrado/orientacao/projetos/dados/frames/negativos'
-POSITIVE_PATH = '/home/gmartins/arquivos/uninove/mestrado/orientacao/projetos/dados/frames/positivos'
-FACTOR = .9  # Factor of reduction of image
-NUM_CLASSES = 2  # Number of classes
-# LIST_NUM_FILTERS = [4, 8, 12, 16]  # List of number of filters
-# LIST_KERNEL_SIZE = [(3, 3), (5, 5)]  # List of filters size
-LIST_NUM_FILTERS = [4, 8]  # List of number of filters
-LIST_KERNEL_SIZE = [(3, 3), (5, 5)]  # List of filters size
-BATCH_SIZE = 256
-EPOCHS = 4  # List of epochs
-TRAIN_PRCNT = 2 / 3  # Percent considered for training data
+    FILE_NAME = 'grid_lenet5_dilation{}.log'.format(datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+    NUM_CLASSES = 2
+    PATH_NEGATIVO = os.path.join(main_path, 'negativos')
+    PATH_POSITIVO = os.path.join(main_path, 'positivos')
 
-# Setting logs
-set_log(file_name=FILE_NAME)
+    # Setting logs
+    set_log(file_name=FILE_NAME, path=main_path)
 
-# Read images
-negative_imgs = read_imgs(NEGATIVE_PATH, FACTOR)
-positive_imgs = read_imgs(POSITIVE_PATH, FACTOR)
+    # Read images
+    negative_imgs = read_imgs(PATH_NEGATIVO, factor)
+    positive_imgs = read_imgs(PATH_POSITIVO, factor)
 
-# Build sets
-x_train, y_train, x_test, y_test = build_sets(negative_imgs, positive_imgs, TRAIN_PRCNT)
+    # Build sets
+    x_train, y_train, x_test, y_test = build_sets(negative_imgs, positive_imgs, train_prcnt)
 
-# Clear lists
-negative_imgs, positive_imgs = None, None
+    # Clear lists
+    negative_imgs, positive_imgs = None, None
 
-# One-hot encode
-y_train = tf.keras.utils.to_categorical(y_train, NUM_CLASSES)
-y_test = tf.keras.utils.to_categorical(y_test, NUM_CLASSES)
+    # One-hot encode
+    y_train = tf.keras.utils.to_categorical(y_train, NUM_CLASSES)
+    y_test = tf.keras.utils.to_categorical(y_test, NUM_CLASSES)
 
-# Grid search parameters
-input_shape = (x_train.shape[1:])
-grid_params = dict(num_filters=LIST_NUM_FILTERS, kernel_size=LIST_KERNEL_SIZE)
-lenet5_keras = KerasClassifier(build_fn=get_lenet5,
-                               input_shape=input_shape,
-                               num_classes=NUM_CLASSES,
-                               layer_type='Dilation2D',
-                               batch_size=BATCH_SIZE,
-                               epochs=EPOCHS,
-                               verbose=1,
-                               workers=100,
-                               use_multiprocessing=True)
-grid = GridSearchCV(estimator=lenet5_keras, param_grid=grid_params, cv=3)
-grid_result = grid.fit(x_train, y_train)
+    # Grid search parameters
+    input_shape = (x_train.shape[1:])
+    grid_params = dict(num_filters=list_num_filter, kernel_size=list_kernel_size)
+    lenet5_keras = KerasClassifier(build_fn=get_lenet5,
+                                   input_shape=input_shape,
+                                   num_classes=NUM_CLASSES,
+                                   layer_type='Dilation2D',
+                                   batch_size=batch_size,
+                                   epochs=epochs,
+                                   verbose=verbose,
+                                   workers=100,
+                                   use_multiprocessing=True)
+    grid = GridSearchCV(estimator=lenet5_keras, param_grid=grid_params, cv=3)
+    grid_result = grid.fit(x_train, y_train)
 
-# Results
-print('Best {} accuracy using {}'.format(grid_result.best_score_, grid_result.best_params_))
-print('All combinations:')
-for acc, params in zip(grid_result.cv_results_['mean_test_score'], grid_result.cv_results_['params']):
-    print('Test accuracy {} with {}'.format(acc, params))
+    # Results
+    logging.info('Best {} accuracy using {}'.format(grid_result.best_score_, grid_result.best_params_))
+    logging.info('All combinations:')
+    for acc, params in zip(grid_result.cv_results_['mean_test_score'], grid_result.cv_results_['params']):
+        logging.info('Test accuracy {} with {}'.format(acc, params))
+
+
