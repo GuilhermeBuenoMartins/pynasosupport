1a7f9031df831b1d1e2f4a6e71b0c19c23eeaedb
gmartins <gui.bue@uninove.edu.br>
2022-11-03 00:31:32 -0300

    GridSearch of Resnet-34.

--- src/functions/model.py
+++ src/functions/model.py
@@ -3,9 +3,10 @@ Functions of support for known models.
 """
 import logging
 
-from tensorflow.keras import Sequential, Input
+from tensorflow.keras import Input, Model, Sequential
 from tensorflow.keras.callbacks import History
-from tensorflow.keras.layers import AveragePooling2D, Conv2D, Dense, Flatten, Layer
+from tensorflow.keras.layers import Activation, AveragePooling2D, Add, BatchNormalization, Conv2D, Dense, Flatten, \
+    GlobalAveragePooling2D, Layer, MaxPooling2D, ZeroPadding2D
 
 from morpholayers.layers import Closing2D, Dilation2D, Erosion2D, Gradient2D, InternalGradient2D, Opening2D, \
     TopHatClosing2D, TopHatOpening2D
@@ -29,6 +30,7 @@ def get_layer(layer_type: str, num_filters: int, kernel_size: tuple, strides=(1,
     :param params: Layer parameters: filter, kernel_size, stride, padding and activation.
     :return: Layer
     """
+
     if layer_type is "Conv2D":
         return Conv2D(num_filters, kernel_size, strides, padding, activation=activation)
     elif layer_type is "Dilation2D":
@@ -72,6 +74,7 @@ def get_lenet5(input_shape: tuple, num_classes: int, layer_type: str, num_filter
     :param layer_params: Layer parameters: filter, kernel_size, stride, padding and activation.
     :return: Sequential model.
     """
+
     model = Sequential()
     model.add(Input(input_shape))
     model.add(get_layer(layer_type, num_filters, kernel_size, strides, padding, activation))
@@ -83,15 +86,139 @@ def get_lenet5(input_shape: tuple, num_classes: int, layer_type: str, num_filter
     model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
     return model
 
-def fit_model(model, data_train:tuple, batch_size:int, epochs:int, verbose, validation_data:tuple) -> History:
-    """
-    The function uses paramter to repass to fit model.
 
-    :param model: The model
-    :param data_train: X and Y training
-    :param batch_size: Number of batch size
-    :param epochs: Number of epochs
-    :param validation_data: The validation data
-    :return: History
+def identity_block(x, filters_num):
     """
-    return model.fit(data_train[0], data_train[1], batch_size, epochs, verbose, validation_data=validation_data)
+    Identity block without downsample
+
+    :param x: input
+    :param filters_num: number of filters
+    :return: output
+    """
+
+    x_skip = x
+    # Layer 1
+    x = Conv2D(filters_num, (3, 3), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    # Layer 2
+    x = Conv2D(filters_num, (3, 3), padding='same')(x)
+    x = BatchNormalization()(x)
+    # Add resisdue
+    x = Add()([x_skip, x])
+    x = Activation('relu')(x)
+    return x
+
+
+def conv_block(x, filters_num):
+    """
+    Convolutional block with downsample
+
+    :param x: input
+    :param filters_num: number of filters
+    :return: output
+    """
+
+    x_skip = x
+    # Layer 1
+    x = Conv2D(filters_num, (3, 3), strides=(2, 2), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    # Layer 2
+    x = Conv2D(filters_num, (3, 3), padding='same')(x)
+    x = BatchNormalization()(x)
+    # Convolution of residue
+    x_skip = Conv2D(filters_num, (1, 1), strides=(2, 2), padding='same')(x_skip)
+    # Add residue
+    x = Add()([x_skip, x])
+    x = Activation('relu')(x)
+    return x
+
+
+def get_func_layer(x, layer_type: str, num_filters: int, kernel_size: tuple, strides=(1, 1), padding='same',
+                   activation=None):
+    """
+    Function to get specific layer with determined parameters.
+
+    :param x: input x
+    :param layer_type: Type of layer:
+        'Conv2D';
+         'Dilation2D';
+          'Erosion2D';
+           'Gradient2D';
+            'InternalGradient2D';
+             'Closing2D';
+              'Opening2D';
+               'TopHatClosing2D';or
+                'TopHatOpening2D'.
+    :param params: Layer parameters: filter, kernel_size, stride, padding and activation.
+    :return: Layer
+    """
+
+    if layer_type is "Conv2D":
+        return Conv2D(num_filters, kernel_size, strides, padding, activation=activation)(x)
+    elif layer_type is "Dilation2D":
+        return Dilation2D(num_filters, kernel_size, strides, padding, activation=activation)(x)
+    elif layer_type is "Erosion2D":
+        return Erosion2D(num_filters, kernel_size, strides, padding, activation=activation)(x)
+    elif layer_type is "Gradient2D":
+        return Gradient2D(num_filters, kernel_size, strides, padding)(x)
+    elif layer_type is "InternalGradient2D":
+        return InternalGradient2D(num_filters, kernel_size, strides, padding)(x)
+    elif layer_type is "Opening2D":
+        return Opening2D(num_filters, kernel_size, strides, padding)(x)
+    elif layer_type is "Closing2D":
+        return Closing2D(num_filters, kernel_size, strides, padding)(x)
+    elif layer_type is "TopHatOpening2D":
+        return TopHatOpening2D(num_filters, kernel_size, strides, padding)(x)
+    elif layer_type is "TopHatClosing2D":
+        return TopHatClosing2D(num_filters, kernel_size, strides, padding)(x)
+    else:
+        logging.critical('Layer type {type} not found.'.format(layer_type))
+    return None
+
+
+def get_renet34(input_shape: tuple, num_classes: int, layer_type: str, num_filters: int, kernel_size: tuple,
+                strides=(1, 1), padding='same', activation=None) -> Model:
+    """
+    The function return model ResNet34.
+
+    :param shape: Input shape
+    :param k: number of classes
+    :return: model ResNet34
+    """
+    # Input block
+    input_model = Input(input_shape)
+    x = ZeroPadding2D((3, 3))(input_model)
+    # Block 1
+    x = get_func_layer(x, layer_type, num_filters, kernel_size, strides, padding, activation)
+    x = Conv2D(64, (7, 7), strides=(2, 2))(x)
+    x = BatchNormalization()(x)
+    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='same')(x)
+    # Convolutional block 2
+    x = identity_block(x, 64)
+    x = identity_block(x, 64)
+    x = identity_block(x, 64)
+    # Convolutional block 3
+    x = conv_block(x, 128)
+    x = identity_block(x, 128)
+    x = identity_block(x, 128)
+    x = identity_block(x, 128)
+    # Convolutional block 4
+    x = conv_block(x, 256)
+    x = identity_block(x, 256)
+    x = identity_block(x, 256)
+    x = identity_block(x, 256)
+    x = identity_block(x, 256)
+    x = identity_block(x, 256)
+    # Convolutional block 5
+    x = conv_block(x, 512)
+    x = identity_block(x, 512)
+    x = identity_block(x, 512)
+    # Output block
+    x = GlobalAveragePooling2D()(x)
+    x = Flatten()(x)
+    output_model = Dense(num_classes, activation='softmax')(x)
+    model = Model(input_model, output_model)
+    model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
+    return model
---
+++ src/grid_resnet34.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env cola-env
+# -*- coding: utf-8 -*-
+"""
+Script of ResNet34 execution
+"""
+import logging
+import os.path
+from datetime import datetime
+
+import tensorflow as tf
+from sklearn.model_selection import GridSearchCV
+from tensorflow.python.keras.wrappers.scikit_learn import KerasClassifier
+
+from functions.configlog import set_log
+from functions.media import read_imgs
+from functions.model import get_renet34
+from functions.sampler import build_sets
+
+
+def main(main_path: str, factor=.9, layer_type='Conv2D', list_num_filters=[4, 8, 12, 16], list_kernel_size=[(3, 3), (5, 5)], batch_size=None,
+         epochs=100, train_prcnt=2 / 3, cpu_mode=False, verbose=1):
+    """
+    Main function to execution the script.
+
+    :param main_path: Images and logs path.
+    :param factor: Factor of images reduction.
+    :param list_num_filters: List of number filters.
+    :param list_kernel_size: List of kernel size.
+    :param batch_size: Batch size value.
+    :param epochs: Epochs value.
+    :param train_prcnt: Training size percent.
+    :return: None
+    """
+
+    FILE_NAME = 'grid_resnet34_{}{}.log'.format(layer_type, datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+    NUM_CLASSES = 2
+    PATH_NEGATIVO = os.path.join(main_path, 'negativos')
+    PATH_POSITIVO = os.path.join(main_path, 'positivos')
+
+    # Setting logs
+    set_log(file_name=FILE_NAME, path=main_path)
+
+    # Read images
+    negative_imgs = read_imgs(PATH_NEGATIVO, factor)
+    positive_imgs = read_imgs(PATH_POSITIVO, factor)
+
+    # Build sets
+    x_train, y_train, x_test, y_test = build_sets(negative_imgs, positive_imgs, train_prcnt)
+
+    # Clear lists
+    negative_imgs, positive_imgs = None, None
+
+    # One-hot encode
+    y_train = tf.keras.utils.to_categorical(y_train, NUM_CLASSES)
+    y_test = tf.keras.utils.to_categorical(y_test, NUM_CLASSES)
+
+    # Grid search parameters
+    input_shape = (x_train.shape[1:])
+    grid_params = dict(num_filters=list_num_filters, kernel_size=list_kernel_size)
+    lenet5_keras = KerasClassifier(build_fn=get_renet34,
+                                   input_shape=input_shape,
+                                   num_classes=NUM_CLASSES,
+                                   layer_type=layer_type,
+                                   batch_size=batch_size,
+                                   epochs=epochs,
+                                   verbose=verbose,
+                                   use_multiprocessing=True
+                                   )
+    grid = GridSearchCV(estimator=lenet5_keras, param_grid=grid_params, cv=10)
+    if cpu_mode:
+        with tf.device('/cpu:0'):
+            grid_result = grid.fit(x_train, y_train)
+
+    # Results
+    logging.info('Best {} accuracy using {}'.format(grid_result.best_score_, grid_result.best_params_))
+    logging.info('All combinations:')
+    for acc, params in zip(grid_result.cv_results_['mean_test_score'], grid_result.cv_results_['params']):
+        logging.info('Test accuracy {} with {}'.format(acc, params))
