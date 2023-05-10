7c0a87725d89057294537c2aa07abbed38b89fd0
Guilherme Bueno Martins <gui.bue@uninove.edu.br>
2023-05-10 13:59:32 -0300

    Outra métricas de avaliação do gridsearch

--- gridsearch.py
+++ gridsearch.py
@@ -8,7 +8,7 @@ class GridSearch:
     cv = None
     model_list = None
     num_classes = None
-    acc_mean_list = None
+    eval_mean_list = None
 
     def __init__(self, model_list: list, num_classes: int = 2, cv: int = 2):
         self.model_list = model_list
@@ -30,14 +30,24 @@ class GridSearch:
             return [None for i in range(len(self.model_list))]
         return callbacks_list
 
-    def eval_fit(self, model, val_x, val_y):
+    def eval_fit(self, model, val_x, val_y, eval_criteria='accuracy'):
         pred_val_y = np.where(model.predict(val_x) >= 0.5, 1, 0)
-        acc = metrics.accuracy_score(val_y, pred_val_y)
-        print('\nFold accuracy: ', acc, '\n')
-        return acc
+        if eval_criteria is 'accuracy':
+            eval_value = metrics.accuracy_score(val_y, pred_val_y)
+            print('\nFold accuracy: ', eval_value, '\n')
+        elif eval_criteria is 'precision':
+            eval_value = metrics.precision_score(val_y, pred_val_y)
+            print('\nFold precision: ', eval_value, '\n')
+        elif eval_criteria is 'recall':
+            eval_value = metrics.recall_score(val_y, pred_val_y)
+            print('\nFold recall: ', eval_value, '\n')
+        else:
+            eval_value = metrics.f1_score(val_y, val_y)
+            print('\nFold F1-score: ', eval_value, '\n')
+        return eval_value
 
     def fit_model(self, model, x, y, batch_size=None, epochs=1, verbose='auto', callbacks=None, val_data=None,
-                  workers=1, use_multiprocessing=False):
+                  workers=1, use_multiprocessing=False, eval_criteria='accuracy'):
         skf = StratifiedKFold(n_splits=self.cv, shuffle=True)
         acc_list = []
         for fold_id, (train_id, val_id) in enumerate(skf.split(x, y)):
@@ -46,22 +56,22 @@ class GridSearch:
             model.fit(train_x, train_y, batch_size, epochs, verbose, callbacks=callbacks,
                                                 validation_data=(val_x, val_y), workers=workers,
                                                 use_multiprocessing=use_multiprocessing)
-            acc_list.append(self.eval_fit(model, val_x, val_y))
+            acc_list.append(self.eval_fit(model, val_x, val_y, eval_criteria=eval_criteria))
         print("Model fitted.")
         return np.mean(acc_list)
 
     def fit_models(self, x, y, batch_size=None, epochs=1, verbose='auto', callbacks_list=None, workers=1,
                    use_multiprocessing=False):
-        self.acc_mean_list = []
+        self.eval_mean_list = []
         callbacks_list = self.treat_callbacks(callbacks_list)
         for model_id, model, callbacks in zip(range(len(self.model_list)), self.model_list, callbacks_list):
             print('Fitting model: ', model_id)
-            acc_mean = self.fit_model(model, x, y, batch_size, epochs, verbose, callbacks, workers, use_multiprocessing)
-            self.acc_mean_list.append(acc_mean)
-            print('\nAccuracy mean: ', acc_mean, '\n\n')
+            eval_mean = self.fit_model(model, x, y, batch_size, epochs, verbose, callbacks, workers, use_multiprocessing)
+            self.eval_mean_list.append(eval_mean)
+            print('\nEvaluation mean: ', eval_mean, '\n\n')
         print('All models was fitted!')
 
     def get_best_model(self):
-        best_model_id = np.argmax(self.acc_mean_list)
+        best_model_id = np.argmax(self.eval_mean_list)
         print('Best model index: ', best_model_id)
         return self.model_list[best_model_id], best_model_id
